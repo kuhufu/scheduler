dd2e67ddedeb5cdb304d58814999894eaef9ee1a
kuhufu <kuhufu@gmail.com>
2019-07-09 01:10:42 +0800

    解决running的数据竞争问题

--- scheduler.go
+++ scheduler.go
@@ -1,36 +1,45 @@
 package scheduler
 
 import (
+	"sync"
 	"time"
 )
 
 type Scheduler struct {
-	start   chan struct{}
-	stop    chan struct{}
-	running bool
+	start       chan struct{}
+	stop        chan struct{}
+	running     bool
+	runningLock *sync.Mutex
 }
 
 func New() *Scheduler {
 	return &Scheduler{
-		start:   make(chan struct{}),
-		stop:    make(chan struct{}),
-		running: false,
+		start:       make(chan struct{}),
+		stop:        make(chan struct{}),
+		running:     false,
+		runningLock: &sync.Mutex{},
 	}
 }
 
 func (s *Scheduler) Stop() {
+	s.runningLock.Lock()
 	if !s.running {
+		s.runningLock.Unlock()
 		return
 	}
 	s.running = false
+	s.runningLock.Unlock()
 	close(s.stop)
 }
 
 func (s *Scheduler) Start() {
+	s.runningLock.Lock()
 	if s.running {
+		s.runningLock.Unlock()
 		return
 	}
 	s.running = true
+	s.runningLock.Unlock()
 	close(s.start)
 }
 
