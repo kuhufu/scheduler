76f3adf1a5dac3aa3a9ac8ed49d77f2f1f5c9652
kuhufu <kuhufu@gmail.com>
2019-07-09 01:11:03 +0800

    添加running的数据竞争测试方法

--- scheduler_test.go
+++ scheduler_test.go
@@ -3,6 +3,7 @@ package scheduler
 import (
 	"fmt"
 	"log"
+	"sync"
 	"testing"
 	"time"
 )
@@ -13,9 +14,6 @@ func TestScheduler_AddIntervalFunc(t *testing.T) {
 
 	s.AddIntervalFunc(time.Second, func() {
 		log.Println("interval")
-	})
-	s.AddTimeoutFunc(time.Second*2, func() {
-		log.Println("timeout")
 	})
 	s.AddTimeoutFunc(time.Second*2, func() {
 		log.Println("timeout")
@@ -35,4 +33,17 @@ func TestFoo(t *testing.T) {
 	<-c
 	<-c
 	fmt.Println("after c")
+}
+
+func TestRunningDataRace(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func() {
+			s.Start()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
